809d6b3d5442cedb3bb4164e6f1b56f268d6562e
wilson <wilsonmiguel2003@gmail.com>
2023-01-10 14:58:49 -0300

    modificaciones en header, about me y projects

--- src/app/models/proyectos.ts
+++ src/app/models/proyectos.ts
@@ -1,13 +1,13 @@
 export class Proyectos {
     id?: number;
-    nombre: string;
     urlimagen: string;
+    nombre: string;
     urlproyecto: string;
     descripcion: string;
 
-    constructor (nombre: string, urlimagen: string, urlproyecto: string, descripcion: string){
-        this.nombre = nombre;
+    constructor (urlimagen: string, nombre: string, urlproyecto: string, descripcion: string){
         this.urlimagen = urlimagen;
+        this.nombre = nombre;
         this.urlproyecto = urlproyecto;
         this.descripcion = descripcion;
 
